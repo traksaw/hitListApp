ef98fa115f86b5019dab9d5e06a53ba46f809417
Waskar Paulino <155832641+traksaw@users.noreply.github.com>
2025-02-24 15:06:40 -0500

    prior to update

---
+++ hitList/src/components/JobListings.jsx
@@ -0,0 +1,22 @@
+import jobs from '/Users/waskar.paulino/Desktop/Personal/ascendProgram/week2/hitListApp/hitList/jobs.json'
+
+const JobListings = () => {
+    console.log(jobs.jobs[2], 'here they are')
+
+    return (
+        <section className="bg-blue-50 px-4 py-10">
+            <div className="container-xl lg:container m-auto">
+                <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+                    Browse Jobs
+                </h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {jobs.jobs.map((job) => (
+                        
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default JobListings
